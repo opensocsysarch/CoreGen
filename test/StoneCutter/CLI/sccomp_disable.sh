d600d21aacf177642face3253a216d2f69f5a7b7
John Leidel <john.leidel@gmail.com>
2018-12-13 20:21:14 +0000

    adding tests for enabling, disabling and listing passes

---
+++ test/StoneCutter/CLI/sccomp_disable.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCCOMP_PATH=$1
+
+if [ ! -f $SCCOMP_PATH/sccomp ]; then
+  echo "COULD NOT FIND $SCCOMP_PATH/sccomp"
+  exit -1
+fi
+
+FILE=test.OPT.sc
+touch $FILE
+echo "# this is a stonecutter source file" >> $FILE 2>&1
+
+$SCCOMP_PATH/sccomp --disable-pass "LICMPass" $FILE
+retVal=$?
+if [ ! $? -eq 0 ]; then
+  echo "$SCCOMP_PATH/sccomp --disable-pass "LICMPass" $FILE failed with return code = $retVal"
+  rm -Rf $FILE
+  exit $retVal
+fi
+
+rm -Rf $FILE
+
+exit 0
