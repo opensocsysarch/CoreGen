746c18bcea6c4927c01daaf58e96ecd3983a752d
John Leidel <john.leidel@gmail.com>
2019-01-21 23:15:39 +0000

    updating CodeGen/SCImpl test harness to compile the stonecutter files

--- README.md
+++ README.md
@@ -231,6 +231,8 @@ make
 * -DBUILD\_COREGEN\_STONECUTTER\_PARSER\_TESTING=ON : Enables the StoneCutter parser test harness
 * -DBUILD\_COREGEN\_STONECUTTER\_PARSER\_KNOWNFAIL\_TESTING=ON : Enables the StoneCutter parser known fail test harness
 * -DBUILD\_COREGEN\_STONECUTTER\_PARSER\_KNOWNPASS\_MULTIFILE\_TESTING=ON : Enables the StoneCutter parser multi-file test harness
+* -DBUILD\_COREGEN\_CODEGEN\_TESTING=ON : Enables the CodeGen test harness
+* -DBUILD\_COREGEN\_CODEGEN\_STONECUTTERIMPL\_TESTING=ON : Enables the CodeGen StoneCutter implementation test harness
 
 ## Contributing
 
--- test/CodeGen/StoneCutterImpl/codegen_impl_test.sh
+++ test/CodeGen/StoneCutterImpl/codegen_impl_test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 CGCLI_PATH=$1
-FILE=$2
+SCCOMP_PATH=$2
+FILE=$3
 OPTS="--chisel"
 
 if [ ! -f $CGCLI_PATH/cgcli ]; then
@@ -18,10 +19,24 @@ DIR=`grep "ProjectRoot" $FILE | awk '{print $2}'`
 
 $CGCLI_PATH/cgcli $OPTS --ir $FILE
 retVal=$?
-if [[  "$retVal" -ne 0 ]]; then
-  echo "$SCCOMP_PATH/cgcli $OPTS --ir $FILE failed with return code = $retVal"
+if [[  "$retValY" -ne 0 ]]; then
+  echo "$CGCLI_PATH/cgcli $OPTS --ir $FILE failed with return code = $retVal"
   exit $retVal
 fi
 
-#rm -Rf $DIR
+ISAs=`grep "ISAName" $FILE | awk '{print $3}' | uniq`
+
+#-- walk through all the stonecutter files and attempt to compile them
+for ISA in $ISAs; do
+  if [ -f $DIR/RTL/stonecutter/$ISA.sc]; then
+    $SCCOMP_PATH/sccomp -p $DIR/RTL/stonecutter/$ISA.sc
+    retValSc=$?
+    if [[  "$retValY" -ne 0 ]]; then
+      echo "$SCCOMP_PATH/sccomp -p $DIR/RTL/stonecutter/$ISA.sc failed with return code = $retValSc"
+      exit $retValSc
+    fi
+  fi
+done;
+
+rm -Rf $DIR
 exit 0
