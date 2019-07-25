acc5269fda09aee2d539ebc5aaa143c4d3834092
John Leidel <john.leidel@gmail.com>
2019-07-25 12:48:15 +0000

    completing first part of the pipe info data class

--- src/StoneCutter/SCPipeInfo.cpp
+++ src/StoneCutter/SCPipeInfo.cpp
@@ -12,8 +12,60 @@
 
 SCPipeInfo::SCPipeInfo(Module *TM, SCOpts *O, SCMsg *M)
   : TheModule(TM), Opts(O), Msgs(M), PCPresent(false) {
-    PCReg.first = "PC";
-    PCReg.second = "SIGTEMP";
+    // initialize the PC register
+    PCReg.first = "SIGTEMP";
+    PCReg.second = "PC";
+
+    // Derive all the register fields and immediates
+    for( auto &Global : TM->getGlobalList() ){
+      // walk all the global variables and retrieve their attribute lists
+      // look for the appropriate set of immediate and register encoding fields
+      AttributeSet AttrSet = Global.getAttributes();
+
+      if( AttrSet.hasAttribute("fieldtype") ){
+        if( AttrSet.getAttribute("fieldype").getValueAsString().str() == "immediate" ){
+          // found an immediate field
+          unsigned Idx = 0;
+          while( AttrSet.hasAttribute("instformat"+std::to_string(Idx)) ){
+            ImmFields.push_back(
+              std::make_pair<std::string,std::string>(
+                AttrSet.getAttribute("instformat"+std::to_string(Idx)).getValueAsString().str(),
+                Global.getName().str()) );
+          }
+        }else if( AttrSet.getAttribute("fieldype").getValueAsString().str() == "immediate" ){
+          // found a register field
+          unsigned Idx = 0;
+          while( AttrSet.hasAttribute("instformat"+std::to_string(Idx)) ){
+            RegFields.push_back(
+              std::make_pair<std::string,std::string>(
+                AttrSet.getAttribute("instformat"+std::to_string(Idx)).getValueAsString().str(),
+                Global.getName().str()) );
+          }
+        }
+        // else, ignore the encoding fields
+      }else if( AttrSet.hasAttribute("register") &&
+                AttrSet.hasAttribute("pc") ){
+        if( AttrSet.getAttribute("pc").getValueAsString().str() == "true" ){
+          // found a pc register
+          PCReg.first  = AttrSet.getAttribute("regclass").getValueAsString().str();
+          PCReg.second = AttrSet.getAttribute("register").getValueAsString().str();
+        }
+      }
+    }
+
+    // Derive the unique set of register fields and immediates
+    for( auto it = ImmFields.begin(); it != ImmFields.end(); ++it ){
+      UniqueImmFields.push_back( it->second );
+    }
+    for( auto it = RegFields.begin(); it != RegFields.end(); ++it ){
+      UniqueRegFields.push_back( it->second );
+    }
+    std::sort( UniqueImmFields.begin(), UniqueImmFields.end() );
+    UniqueImmFields.erase( std::unique( UniqueImmFields.begin(), UniqueImmFields.end() ),
+                           UniqueImmFields.end() );
+    std::sort( UniqueRegFields.begin(), UniqueRegFields.end() );
+    UniqueRegFields.erase( std::unique( UniqueRegFields.begin(), UniqueRegFields.end() ),
+                           UniqueRegFields.end() );
 }
 
 SCPipeInfo::~SCPipeInfo(){
