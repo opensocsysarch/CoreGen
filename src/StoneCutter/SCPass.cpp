8d673155a2703bdcd5164bfe86c6c080c6b03796
John Leidel <john.leidel@gmail.com>
2020-01-21 16:46:20 +0000

    fix for Issue #226; SCPass was using an incorrectly spelled attribute name

--- src/StoneCutter/SCPass.cpp
+++ src/StoneCutter/SCPass.cpp
@@ -207,7 +207,7 @@ std::vector<std::string> SCPass::GetRegClassInstTypes(std::string InstFormat){
       if( AttrSet.getAttribute("fieldtype").getValueAsString().str() == "register" ){
         unsigned Idx = 0;
         while( AttrSet.hasAttribute("instformat"+std::to_string(Idx)) ){
-          if( AttrSet.getAttribute("Instformat"+std::to_string(Idx)).getValueAsString().str() ==
+          if( AttrSet.getAttribute("instformat"+std::to_string(Idx)).getValueAsString().str() ==
               InstFormat )
             Fields.push_back(
               AttrSet.getAttribute("regclasscontainer"+std::to_string(Idx)).getValueAsString().str() );
