3f6cdf2283f39b717b87bdd02899ccdb07c0024e
John Leidel <john.leidel@gmail.com>
2020-01-27 02:49:44 +0000

    removing dead NodeCodegen for for register classes

--- src/CoreGenCodegen/NodeCodegen/RegClassCG.cpp
+++ src/CoreGenCodegen/NodeCodegen/RegClassCG.cpp
@@ -69,47 +69,6 @@ bool RegClassCG::WriteRegClass(std::ofstream &O ){
 
   return true;
 }
-#if 0
-bool RegClassCG::WriteRegClass(std::ofstream &O ){
-  // write the comment
-  O << "//-- " << Node->GetName() << " Register Class " << std::endl;
-  O << "class " << CGRemoveDot(Node->GetName()) << "RFileIO(implicit val conf: "
-    << CGRemoveDot(Proj->GetProjName()) << "Configuration) extends Bundle()" << std::endl
-    << "{" << std::endl
-    << "\tval rs1_addr = Input(UInt(5.W))" << std::endl
-    << "\tval rs1_data = Output(UInt(conf.max_width.W))" << std::endl
-    << "\tval rs2_addr = Input(UInt(5.W))" << std::endl
-    << "\tval rs2_data = Output(UInt(conf.max_width.W))" << std::endl
-    << "\tval dm_addr = Input(UInt(5.W))" << std::endl
-    << "\tval dm_rdata = Output(UInt(conf.max_width.W))" << std::endl
-    << "\tval dm_wdata = Input(UInt(conf.max_width.W))" << std::endl
-    << "\tval dm_en = Input(Bool())" << std::endl
-    << "\tval waddr    = Input(UInt(5.W))" << std::endl
-    << "\tval wdata    = Input(UInt(conf.max_width.W))" << std::endl
-    << "\tval wen      = Input(Bool())" << std::endl
-    << "}" << std::endl << std::endl;
-
-  O << "class " << CGRemoveDot(Node->GetName()) << "(implicit val conf: "
-    << CGRemoveDot(Proj->GetProjName()) << "Configuration) extends Module" << std::endl
-    << "{" << std::endl
-    << "\tval io = IO(new " << CGRemoveDot(Node->GetName()) << "RFileIO())" << std::endl
-    << "\tval regfile = Mem( UInt(conf." << CGRemoveDot(Node->GetName())
-    << "_numregs.W), UInt(conf." << CGRemoveDot(Node->GetName()) << "_maxwidth.W))" << std::endl
-    << "\twhen (io.wen && (io.waddr =/= 0.U))" << std::endl
-    << "\t{" << std::endl
-    << "\t\tregfile(io.waddr) := io.wdata" << std::endl
-    << "\t}" << std::endl << std::endl
-    << "\twhen (io.dm_en && (io.dm_addr =/= 0.U))" << std::endl
-    << "\t{" << std::endl
-    << "\t\tregfile(io.dm_addr) := io.dm_wdata" << std::endl
-    << "\t}" << std::endl << std::endl
-    << "\tio.rs1_data := Mux((io.rs1_addr =/= 0.U), regfile(io.rs1_addr), 0.U)" << std::endl
-    << "\tio.rs2_data := Mux((io.rs2_addr =/= 0.U), regfile(io.rs2_addr), 0.U)" << std::endl
-    << "\tio.dm_rdata := Mux((io.dm_addr =/= 0.U), regfile(io.dm_addr), 0.U)" << std::endl
-    << "}" << std::endl << std::endl;
-  return true;
-}
-#endif
 
 bool RegClassCG::Execute(){
 
