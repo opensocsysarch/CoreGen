096a9b47a5efcba58233b4fd5caf0eb1818afdec
John Leidel <john.leidel@gmail.com>
2020-06-03 13:45:40 +0000

    adding tests for scheduler Yaml parameter

---
+++ test/Yaml/Reader/yaml_reader_test_104.cpp
@@ -0,0 +1,37 @@
+//
+// _YAML_READER_TEST104_CPP_
+//
+// Copyright (C) 2017-2020 Tactical Computing Laboratories, LLC
+// All Rights Reserved
+// contact@tactcomplabs.com
+//
+// See LICENSE in the top level directory for licensing details
+// 
+//
+
+#include <iostream>
+#include "CoreGen/CoreGenBackend/CoreGenBackend.h"
+
+std::string PROJNAME = "TEST104";
+std::string PROJROOT = "./";
+std::string ARCHROOT = "./";
+std::string PROJYAML = "../TEST104.yaml";
+
+int main( int argc, char **argv ){
+  CoreGenBackend *CG = new CoreGenBackend(PROJNAME,
+                                          PROJROOT,
+                                          ARCHROOT);
+  if( !CG ){
+    std::cout << "ERROR : FAILED TO CREATE COREGEN OBJECT" << std::endl;
+    return -1;
+  }
+
+  if( !CG->ReadIR( PROJYAML ) ){
+    std::cout << "ERROR : FAILED TO READ YAML IR:" << CG->GetErrStr() << std::endl;
+    delete CG;
+    return -1;
+  }
+
+  delete CG;
+  return 0;
+}
