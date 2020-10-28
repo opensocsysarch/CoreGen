488d469ccca393eee33763f2aeeb1b702c428eff
John Leidel <john.leidel@gmail.com>
2020-10-28 12:02:27 -0500

    adding more options for the command line interface

--- include/CoreGen/Poar/PoarOpts.h
+++ include/CoreGen/Poar/PoarOpts.h
@@ -32,14 +32,23 @@
 #endif
 
 #ifndef POAR_PATCH_VERSION
-#define POAR_PATCH_VERSION 1
+#define POAR_PATCH_VERSION 0
 #endif
 
 class PoarOpts{
 private:
   // private variables
-  int argc;           ///< PoarOpts: ARGC command line info
-  char **argv;        ///< PoarOpts: ARGV command line info
+  int argc;             ///< PoarOpts: ARGC command line info
+  char **argv;          ///< PoarOpts: ARGV command line info
+  std::string YamlFile; ///< PoarOpts: Yaml input file
+  std::string SCFile;   ///< PoarOpts: StoneCutter input file
+  std::string Config;   ///< PoarOpts: configuration file
+  std::string OutFile;  ///< PoarOpts: output file
+
+  bool TextOutput;      ///< PoarOpts: defines textual output
+  bool YamlOutput;      ///< PoarOpts: defines yaml output
+  bool LatexOutput;     ///< PoarOpts: defines latex output
+  bool XmlOutput;       ///< PoarOpts: defines xml output
 
   // private functions
 
@@ -58,6 +67,27 @@ public:
 
   /// PoarOpts: Parse the command line options
   bool ParseOpts(bool *isHelp);
+
+  /// PoarOpts: Retrieve the configuration file name
+  std::string GetConfig() { return Config; }
+
+  /// PoarOpts: Returns whether the text output is selected
+  bool IsTextOutput() { return TextOutput; }
+
+  /// PoarOpts: Returns whether the yaml output is selected
+  bool IsYamlOutput() { return YamlOutput; }
+
+  /// PoarOpts: Returns whether the latex output is selected
+  bool IsLatexOutput() { return LatexOutput; }
+
+  /// PoarOpts: Returns whether the XML output is selected
+  bool IsXmlOutput() { return XmlOutput; }
+
+  /// PoarOpts: Retrieves the output file name
+  std::string GetOutFile() { return OutFile; }
+
+  /// PoarOpts: Retrieves the design input file
+  std::string GetDesignFile() { return YamlFile; }
 };
 
 #endif // _POAROPTS_H_
--- src/Poar/PoarOpts.cpp
+++ src/Poar/PoarOpts.cpp
@@ -11,7 +11,9 @@
 #include "CoreGen/Poar/PoarOpts.h"
 
 PoarOpts::PoarOpts(int a, char **v)
-  : argc(a), argv(v) { }
+  : argc(a), argv(v),
+    TextOutput(true), YamlOutput(false),
+    LatexOutput(false), XmlOutput(false) { }
 
 PoarOpts::~PoarOpts(){
 }
@@ -25,10 +27,21 @@ void PoarOpts::PrintVersion(){
 }
 
 void PoarOpts::PrintHelp(){
-  std::cout << "poar [Options] /path/to/design.yaml" << std::endl;
+  std::cout << "poar [Options] --design /path/to/design.yaml" << std::endl;
   std::cout << "Options:" << std::endl;
   std::cout << "\t-h|-help|--help                     : Print the help menu" << std::endl;
-  std::cout << "\t-v|-version|--version                : Print the version information" << std::endl;
+  std::cout << "\t-v|-version|--version               : Print the version information" << std::endl;
+  std::cout << "\t-c|-config|--config FILE.yaml       : Set the configuration file" << std::endl;
+  std::cout << "\t-o|-outfile|--outfile FILE          : Sets the output file." << std::endl;
+  std::cout << std::endl;
+  std::cout << "Input Files:" << std::endl;
+  std::cout << "\t--design /path/to/design.yaml       : Set the CoreGen design input file" << std::endl;
+  std::cout << std::endl;
+  std::cout << "Output Formats:" << std::endl;
+  std::cout << "\t--text                              : Text output (default)" << std::endl;
+  std::cout << "\t--yaml                              : Yaml output" << std::endl;
+  std::cout << "\t--latex                             : Latex output" << std::endl;
+  std::cout << "\t--xml                               : XML output" << std::endl;
 }
 
 bool PoarOpts::ParseOpts(bool *isHelp){
@@ -49,10 +62,59 @@ bool PoarOpts::ParseOpts(bool *isHelp){
       *isHelp = true;
       PrintVersion();
       return true;
+    }else if( (s=="-c") || (s=="-config") || (s=="--config") ){
+      if( i+1 > (argc-1) ){
+        std::cout << "Error : --config requires an argument" << std::endl;
+        return false;
+      }
+      std::string S(argv[i+1]);
+      Config = S;
+      i++;
+    }else if( (s=="-o") || (s=="-outfile") || (s=="--outfile") ){
+      if( i+1 > (argc-1) ){
+        std::cout << "Error : --outfile requires an argument" << std::endl;
+        return false;
+      }
+      std::string S(argv[i+1]);
+      OutFile = S;
+      i++;
+    }else if( s=="--design" ){
+      if( i+1 > (argc-1) ){
+        std::cout << "Error : --design requires an argument" << std::endl;
+        return false;
+      }
+      std::string S(argv[i+1]);
+      YamlFile = S;
+      i++;
+    }else if( s=="--text" ){
+      TextOutput = true;
+      YamlOutput = false;
+      LatexOutput = false;
+      XmlOutput = false;
+    }else if( s=="--yaml" ){
+      TextOutput = false;
+      YamlOutput = true;
+      LatexOutput = false;
+      XmlOutput = false;
+    }else if( s=="--latex" ){
+      TextOutput = false;
+      YamlOutput = false;
+      LatexOutput = true;
+      XmlOutput = false;
+    }else if( s=="--xml" ){
+      TextOutput = false;
+      YamlOutput = false;
+      LatexOutput = false;
+      XmlOutput = true;
     }else{
       std::cout << "Error : unknown option: " << s << std::endl;
       return false;
     }
+  }
+
+  if( YamlFile.length() == 0 ){
+    std::cout << "Error : no design inputs specified; see the --design option" << std::endl;
+    return false;
   }
 
   return true;
