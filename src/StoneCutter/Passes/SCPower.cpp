5ba8d6fa2f935ffc2dad4bc3ea10516299840d9f
brick <rkabrick@udel.edu>
2020-11-23 14:52:28 -0500

    Initiation Interval stuff

--- src/StoneCutter/Passes/SCPower.cpp
+++ src/StoneCutter/Passes/SCPower.cpp
@@ -29,30 +29,55 @@ bool SCPower::Execute(){
     return false;
   }
 
-  // retrieve the pipeline stage vector
-  // InstVect = SigMap->GetInstVect();
+  if( !SigMap ){
+    this->PrintMsg(L_ERROR, "SigMap is null");
+    return false;
+  }
 
-  // //std::cout << sizeof(PipeVect) << std::endl;
+  PipeVect = SigMap->GetPipeVect();
+  InstVect = SigMap->GetInstVect();
 
-  // // allocate the matrix
-  // if( !AllocMat() ){
-  //   this->PrintMsg( L_ERROR, "Could not allocate matrix" );
-  //   return false;
-  // }
-
-  // // build the pipeline matrix
-  // if( !BuildMat() ){
-  //   this->PrintMsg( L_ERROR, "Could not build matrix representation of pipeline" );
-  //   FreeMat();
-  //   return false;
-  // }
-
-  // //PrintAdjMat();
-  // // free the matrix
-  // if( !FreeMat() ){
-  //   this->PrintMsg( L_ERROR, "Could not free adjacency matrix" );
-  //   return false;
-  // }
-
+  for( unsigned i=0; i<InstVect.size(); i++ ){
+    // Get vector of signals for every instruction
+    std::vector<SCSig *> SigVect = SigMap->GetSigVect(InstVect[i]);
+    unsigned DupArithCount = 0;
+    // For each SigVect, see if any have duplicate ALU_* symbols (ie. EXE stage for more than 1 cycle)
+    for( unsigned j=1; j<SigVect.size(); j++){
+      SigType PrevSigType = SigVect[j-1]->GetType();
+      // TODO: Could probably turn this into a signal line Ternary
+      if( SigVect[j]->isALUSig() && SigVect[j]->GetType() == PrevSigType ){
+        DupArithCount++;
+      }
+      else{ 
+        continue;
+      }
+    }
+    std::cout << InstVect[i] << ": " << DupArithCount << std::endl;
+  }
   return true;
 }
+
+
+/*** Crap Code ***/
+
+ // ! Accidentally recreated getting cycle count w/out realizing only prob was w/ VLSI & SuperScalar *//
+  // std::cout <<SigMap->GetSigVect(SigMap->GetSignal(0)->GetInst()).size() << std::endl;
+  // PipeVect = SigMap->GetPipeVect();
+  // InstVect = SigMap->GetInstVect();
+
+  // unsigned cyclesForInst = 0; 
+  // // Try to get accurate cycle count for instructions (For RISC, should be 5)
+  // for (unsigned i=0; i<SigMap->GetNumSignals(); i++){
+  //   //SCSig *Next = SigMap->GetSignal(i+1);
+  //   unsigned cyclesForInst;
+  //   if (SigMap->GetSignal(i)->GetInst() == SigMap->GetSignal(i+1)->GetInst()){
+  //     cyclesForInst++;
+  //     continue;
+  //   }
+  //   else{
+  //     // std::cout << SigMap->GetSignal(i)->GetInst() << ": " << cyclesForInst << std::endl;
+  //     cyclesForInst = 0;
+  //     continue;
+  //   }
+
+  // }
